628f8003bf49ae7df3aef7ee5cd20681c570c2fd
Nubianprince <evereth@hotmail.com>
2019-11-27 11:20:11 -0500

    Create aicp_gt58wifi.mk

---
+++ aicp_gt58wifi.mk
@@ -0,0 +1,35 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy Tab A
+
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/aicp.mk)
+
+# Product makefile
+$(call inherit-product, device/samsung/gt58wifi/device.mk)
+
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 768
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gt58wifi
+PRODUCT_NAME :=aicp_gt58wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T350
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+
+# Set build fingerprint & Product Name.
+BUILD_FINGERPRINT=samsung/gt58wifixx/gt58wifi:7.1.1/NMF26X/T350XXU1CQJ5:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  TARGET_DEVICE=gt58wifi \
+  PRODUCT_NAME=gt58wifixx \
+  PRIVATE_BUILD_DESC="gt58wifixx-user 7.1.1 NMF26X T350XXU1CQJ5 release-keys"
+
